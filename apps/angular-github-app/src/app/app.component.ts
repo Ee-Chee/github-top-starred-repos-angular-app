3a8e04f7c9c83c720f31e93e60c720d20a3eb113
ecleng <ecleng@nettrek.de>
2024-04-28 15:22:38 +0200

    feature(aga): nx setup, eslint, prettier, stylelint, precommit hook, angular material, base stylings

--- apps/angular-github-app/src/app/app.component.ts
+++ apps/angular-github-app/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, MatButtonModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
